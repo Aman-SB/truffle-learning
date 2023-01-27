0984980c39c0bde6be738a09f88031a8fd0ce890
Aman Bisht <bishtaman60@gmail.com>
2023-01-27 14:06:50 +0530

    how to deploy dapp in truffle

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract SimpleStorage{
+    uint256 a;
+    uint256 b;
+     function setter (uint256 _a) public{
+        a=_a;
+     }
+
+     function getter() public view returns (uint256) {
+        return a;
+     }
+}
